023f81829862b62cc78310bb566ea865aceea398
Thibault Barolat Massole <Thibault.Barolat.Massole@fr.ey.com>
2019-01-29 12:42:01 +0100

    Fixed: Namespace

--- DateTextTypeBundle.php
+++ DateTextTypeBundle.php
@@ -1,12 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: TBarolat
- * Date: 29/01/2019
- * Time: 11:14
- */
 
-namespace App\DateTextTypeBundle;
+namespace ThBM\DateTextTypeBundle;
 
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
